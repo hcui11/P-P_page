eafe9f7ac1934cc3d51755d500048099fb50e811
Jacky Zheng <jz675@cornell.edu>
2018-09-29 10:34:05 -0400

    will it work?

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
 /*jshint esversion: 6 */
 
-const svg = d3.select("body").append("div").classed("svg-container", true) //container class to make it responsive
+const svg = d3.select("body") //container class to make it responsive
    .append("svg")
    //responsive SVG needs these 2 attributes and no width and height attr
    .attr("preserveAspectRatio", "xMinYMin slice")
    .attr("viewBox", "0 0 600 400")
    //class to make it responsive
-   .classed("svg-content-responsive", true);
+   .classed("svg-content-responsive", true)
+   .on("mouseClick", hovering);
 var Pi2 = 2*Math.PI;
-var data = [Pi2/5, 2*Pi2/5, 3*Pi2/5, 4*Pi2/5, Pi2];
-const NumArc = 4;
+//data containing information of each arc
+var data = [{"location" : Pi2/5, "color":"red"},
+            {"location" : 2*Pi2/5, "color":"blue"},
+            {"location" : 3*Pi2/5, "color":"green"},
+            {"location" : 4*Pi2/5, "color":"orange"},
+            {"location" : Pi2, "color":"magenta"}];
+//creating the arc function
 var arc = d3.arc()
-    .startAngle(function (d) {return d- Pi2/5})
+    .startAngle(function (d) {return d.location - Pi2/5;})
     .outerRadius(130)
     .innerRadius(100)
     .cornerRadius(2)
     .padAngle(0.005)
-    .endAngle(function (d) {return d});
+    .endAngle(function (d) {return d.location;});
 
 var Paths = svg.selectAll("path").data(data);
-var MakeArcs = Paths.enter().append("path").attr("d", arc).attr("transform", "translate(280,150)");
+//creating the actual arcs
+var MakeArcs = Paths.enter().append("path")
+                    .attr("d", arc)
+                    .attr("fill", function (d) {return d.color;})
+                    .on("mousemove", function () {console.log("hellO");})
+                    .attr("transform", "translate(280,150)");
+
+svg.append("circle").attr("cy", 100).attr("cx", 100).attr("r", 10).on("mouseOver", hovering);
+function hovering(){
+  d3.select(this).attr("fill", "red");
+  console.log("hovering");
+}
