8941f32b2ff74638d336521e5c788f0044972a89
Jacky Zheng <jz675@cornell.edu>
2018-09-29 11:01:09 -0400

    working?

--- script.js
+++ script.js
@@ -6,8 +6,7 @@ const svg = d3.select("body") //container class to make it responsive
    .attr("preserveAspectRatio", "xMinYMin slice")
    .attr("viewBox", "0 0 600 400")
    //class to make it responsive
-   .classed("svg-content-responsive", true)
-   .on("mouseClick", hovering);
+   .classed("svg-content-responsive", true);
 var Pi2 = 2*Math.PI;
 //data containing information of each arc
 var data = [{"location" : Pi2/5, "color":"red"},
@@ -18,8 +17,8 @@ var data = [{"location" : Pi2/5, "color":"red"},
 //creating the arc function
 var arc = d3.arc()
     .startAngle(function (d) {return d.location - Pi2/5;})
-    .outerRadius(130)
-    .innerRadius(100)
+    .outerRadius(160)
+    .innerRadius(130)
     .cornerRadius(2)
     .padAngle(0.005)
     .endAngle(function (d) {return d.location;});
@@ -29,11 +28,10 @@ var Paths = svg.selectAll("path").data(data);
 var MakeArcs = Paths.enter().append("path")
                     .attr("d", arc)
                     .attr("fill", function (d) {return d.color;})
-                    .on("mousemove", function () {console.log("hellO");})
+                    .on("click", Onclick)
                     .attr("transform", "translate(280,150)");
-
-svg.append("circle").attr("cy", 100).attr("cx", 100).attr("r", 10).on("mouseOver", hovering);
-function hovering(){
+function Onclick(){
+  MakeArcs.attr("d", arc).attr("fill", function (d) {return d.color;});
   d3.select(this).attr("fill", "red");
   console.log("hovering");
 }
