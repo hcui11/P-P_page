90078964eb82dac2da1a415bd4c89321a99adbb6
Jacky Zheng <jackyzheng.zheng@gmail.com>
2018-10-04 22:30:34 -0400

    template done

--- script.js
+++ script.js
@@ -4,15 +4,14 @@ var radius = 160;
 var offsetx = 280;
 var offsety = radius + 40;
 const svg = d3.select("body") //container class to make it responsive
-    .append("svg")
-    //responsive SVG needs these 2 attributes and no width and height attr
-    .attr("preserveAspectRatio", "xMinYMin slice")
-    .attr("viewBox", "0 0 600 400")
-    //class to make it responsive
-    .classed("svg-content-responsive", true);
-var Pi2 = 2 * Math.PI;
+   .append("svg")
+   //responsive SVG needs these 2 attributes and no width and height attr
+   .attr("preserveAspectRatio", "xMinYMin slice")
+   .attr("viewBox", "0 0 600 400")
+   //class to make it responsive
+   .classed("svg-content-responsive", true);
+var Pi2 = 2*Math.PI;
 //data containing information of each arc
-<<<<<<< HEAD
 var data = [{"location" : Pi2/8, "color":"red", "link":"img/image-1.jpg", "text": "text1"},
             {"location" : 2*Pi2/8, "color":"blue", "link":"img/image-2.jpg", "text": "text2"},
             {"location" : 3*Pi2/8, "color":"green", "link":"img/image-3.jpg", "text": "text3"},
@@ -36,26 +35,6 @@ var arc2 = d3.arc()
     .cornerRadius(2)
     .padAngle(0.005)
     .endAngle(function (d) {return d.location;});
-=======
-var data =
-    [{ "location": Pi2 / 8, "color": "red" },
-    { "location": 2 * Pi2 / 8, "color": "blue" },
-    { "location": 3 * Pi2 / 8, "color": "green" },
-    { "location": 4 * Pi2 / 8, "color": "orange" },
-    { "location": 5 * Pi2 / 8, "color": "magenta" },
-    { "location": 6 * Pi2 / 8, "color": "coral" },
-    { "location": 7 * Pi2 / 8, "color": "cyan" },
-    { "location": Pi2, "color": "orchid" }];
-//creating the arc function
-var arc = d3.arc()
-    .startAngle(function (d) { return d.location - Pi2 / 8; })
-    .outerRadius(160)
-    .innerRadius(130)
-    .cornerRadius(2)
-    .padAngle(0.005)
-    .endAngle(function (d) { return d.location; });
-
->>>>>>> 9c4154ba3c1d5683247fd71b4448f018164cf5f7
 var Paths = svg.selectAll("path").data(data);
 //creating the actual arcs
 
@@ -82,7 +61,6 @@ var image = svg.append("circle")
                 .attr("r", radius)
                 .attr("opacity", 1);
 var MakeArcs = Paths.enter().append("path")
-<<<<<<< HEAD
                     .attr("id", function (d,i) {return "Path" + i;})
                     .attr("d", arc)
                     .attr("fill", function (d) {return d.color;})
@@ -114,14 +92,4 @@ function mouseover() {
 }
 function mouseout(){
   d3.select(this).attr("opacity", 1);
-=======
-    .attr("d", arc)
-    .attr("fill", function (d) { return d.color; })
-    .on("click", Onclick)
-    .attr("transform", "translate(320,180)");
-function Onclick() {
-    MakeArcs.attr("d", arc).attr("fill", function (d) { return d.color; });
-    d3.select(this).attr("fill", "red");
-    console.log("hovering");
->>>>>>> 9c4154ba3c1d5683247fd71b4448f018164cf5f7
 }
